7d74ba4f55e0e9bb11b5365112618ecf551e62ce
Islam <islam99kz@gmail.com>
2022-10-18 16:49:56 +0600

    add google tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,7 @@ import translationEN from 'utils/locales/en.json';
 import translationRU from 'utils/locales/ru.json';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import Script from 'next/script';
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
@@ -27,7 +28,7 @@ i18n
     lng: 'en',
     fallbackLng: 'en',
     debug: process.env.NODE_ENV === 'production' ? false : true
-});
+  });
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -39,12 +40,27 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, [locale]);
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <GlobalLayout>
-        <Component {...pageProps} />
-      </GlobalLayout>
-    </ThemeProvider>
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-QJ0D1VSTY5"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-QJ0D1VSTY5');
+        `}
+      </Script>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <GlobalLayout>
+          <Component {...pageProps} />
+        </GlobalLayout>
+      </ThemeProvider>
+    </>
   )
 }
 

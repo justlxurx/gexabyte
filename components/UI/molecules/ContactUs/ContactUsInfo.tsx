f81f394900b26610d0a96b2b144e426b14f920f0
Soba4kaxbm <Soba4kaxbm@mail.ru>
2022-12-27 18:56:39 +0600

    Change url contactUS

--- components/UI/molecules/ContactUs/ContactUsInfo.tsx
+++ components/UI/molecules/ContactUs/ContactUsInfo.tsx
@@ -41,7 +41,7 @@ const ContactUsInfo = () => {
         fontSize: '16px',
         fontWeight: 600,
       }}>{t('contactUsForm.address')}</Typography>
-      <a href='https://go.2gis.com/9h3gey'>
+      <a href='https://2gis.kz/almaty/search/tech%20culture/firm/70000001054312231/76.929396%2C43.230696?m=76.929749%2C43.230799%2F17.98'>
         <Typography sx={{
           textDecoration: 'underline',
           fontSize: '16px',

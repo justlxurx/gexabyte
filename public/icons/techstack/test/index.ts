1ec9e71dcc4655ddedd1dd411878f3f74d6a3e50
Soba4kaxbm <Soba4kaxbm@mail.ru>
2022-12-27 19:22:10 +0600

    Fix main page error (space in url image)

---
+++ public/icons/techstack/test/index.ts
@@ -0,0 +1,70 @@
+import angularIcon from './angularIcon.svg'
+import avalancheIcon from './avalancheIcon.svg'
+import binanceIcon from './binanceIcon.svg'
+import bunnyIcon from './bunnyIcon.svg'
+import compoundIcon from './compoundIcon.svg'
+import ellipseIcon from './ellipseIcon.svg'
+import fantomIcon from './fantomIcon.svg'
+import ghostIcon from './ghostIcon.svg'
+import javaIcon from './javaIcon.svg'
+import jsIcon from './jsIcon.svg'
+import kotlinIcon from './kotlinIcon.svg'
+import metaMaskIcon from './metaMaskFoxIcon.svg'
+import phpIcon from './phpIcon.svg'
+import ponyIcon from './ponyIcon.svg'
+import pythonIcon from './python.svg'
+import raydiumIcon from './raydiumIcon.svg'
+import reactIcon from './reactIcon.svg'
+import rustIcon from './rustIcon.svg'
+import solanaIcon from './solanaIcon.svg'
+import solidityIcon from './solidityIcon.svg'
+import swiftIcon from './swiftIcon.svg'
+import tsIcon from './tsIcon.svg'
+import VLXIcon from './velasvlxIcon.svg'
+import vueIcon from './vueIcon.svg'
+import python from './pythonIcon.svg'
+import all from './All.svg'
+
+
+
+export const allLogo = [
+    angularIcon,
+    phpIcon,
+    bunnyIcon,
+    ellipseIcon,
+    ponyIcon,
+    compoundIcon,
+    binanceIcon,
+    solanaIcon,
+    pythonIcon,
+    raydiumIcon,
+    swiftIcon,
+    javaIcon,
+    jsIcon,
+    kotlinIcon,
+    metaMaskIcon,
+    reactIcon,
+    rustIcon,
+    solidityIcon,
+    tsIcon,
+    avalancheIcon,
+    fantomIcon,
+    VLXIcon,
+    vueIcon,
+    ghostIcon,
+    all,
+    angularIcon,
+    bunnyIcon,
+    ellipseIcon,
+    phpIcon,
+    ponyIcon,
+    compoundIcon,
+    binanceIcon,
+    solanaIcon,
+    python,
+    raydiumIcon,
+    swiftIcon,
+    javaIcon,
+    jsIcon,
+
+]
